991db720296747d4b8a9bc95b91b0266b4995883
NelsonHenryCampos <nelsino.123@gmail.com>
2019-10-01 00:51:48 -0300

    AlertsPages

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:checkair/ui/AlertsPage/GreenPage.dart';
+import 'package:checkair/ui/AlertsPage/YellowPage.dart';
+
+import 'ui/AlertsPage/RedPage.dart';
 import 'ui/SlidePages/CircularBar.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() {       
   runApp(MaterialApp(
     home: CircularBar(),
   ));
